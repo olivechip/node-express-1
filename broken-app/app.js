2d6b8b834f7af8d9214af7adb5838118c8bd4f42
Oliver <oliver@MSI>
2023-10-26 20:30:15 -0700

    completed broken-app

--- broken-app/app.js
+++ broken-app/app.js
@@ -1,18 +1,36 @@
 const express = require('express');
-let axios = require('axios');
-var app = express();
+const axios = require('axios');
+const app = express();
 
-app.post('/', function(req, res, next) {
+app.use(express.json());
+
+app.get('/', async function (req, res, next){
   try {
-    let results = req.body.developers.map(async d => {
-      return await axios.get(`https://api.github.com/users/${d}`);
-    });
-    let out = results.map(r => ({ name: r.data.name, bio: r.data.bio }));
-
-    return res.send(JSON.stringify(out));
-  } catch {
-    next(err);
+    let devs = req.query.developers;
+    if (!devs){
+      return res.status(400).json({msg: 'Please provide a list of developers.'})
+    } 
+      devs = devs.split(',');
+  
+      const devData = await Promise.all(
+        devs.map(async function(username){
+          try {
+            const response = await axios.get(`https://api.github.com/users/${username}`);
+            if (response.status === 200){
+              const {name, bio} = response.data;
+              return {name, bio};
+            }
+          } catch (err){
+            return {name: null, bio: null};
+          }
+        })
+      )
+      res.json(devData);
+  } catch (err){
+    return res.status(500).json({error: "Internal Server Error"});
   }
-});
+})
 
-app.listen(3000);
+app.listen(port=3000, function(){
+  console.log(`Starting server on port ${port}`);
+});
